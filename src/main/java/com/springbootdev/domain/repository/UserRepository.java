fb2d9490e17ab9b44362ae43a78f0cb37afced6f
Chathuranga Tennakoon <chathuranga@cyrup.sg>
2017-12-31 14:38:01 +0530

    intial commit -  adding all the project files

---
+++ src/main/java/com/springbootdev/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.springbootdev.domain.repository;
+
+import com.springbootdev.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>
+{
+
+}
